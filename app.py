fe7eccdbd6ff7ca81bec9d1232f8d3e8fa178691
DESKTOP-7G62N8C\한국인 <ohnuri97@gmail.com>
2022-05-11 14:51:23 +0900

    로그인 버튼 클릭 시 검사 부분 구현

---
+++ app.py
@@ -0,0 +1,43 @@
+from pymongo import MongoClient
+import certifi
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+SECRET_KEY = 'SPARTA'
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.3rrj5.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    #로그인 버튼 클릭 시
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
