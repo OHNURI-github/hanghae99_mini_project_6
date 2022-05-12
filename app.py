018a770f507342c900adc01aaf032775db72bde4
younhoso <thdbsgh3443@naver.com>
2022-05-12 16:02:26 +0900

    post wirite style update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'SPARTA'
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.3rrj5.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.3rrj5.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/login')
